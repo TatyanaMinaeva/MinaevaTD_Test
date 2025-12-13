3e1d15a112472702ffc23ab35f624193759763c5
TatyanaM <minaeva-td@mail.ru>
2025-12-13 09:07:42 +0300

    Chain of Invocations

--- src/test/java/user/User.java
+++ src/test/java/user/User.java
@@ -1,8 +1,15 @@
 package user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class User {
     private final String email;
     private final String password;
+
+    /*  //заменили эти конструкторы на @Getter и @AllArgsConstructor
 
     public User(String email, String password) {
         this.email = email;
@@ -15,5 +22,5 @@ public class User {
 
     public String getPassword() {
         return password;
-    }
+    }*/
 }
